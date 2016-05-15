4adc66ed8955f3fddeb43a43f4c215992e1ba842
Nikolay Popov <mail@lifebits.ru>
2016-05-15 18:33:21 +0700

    Error display at 404 state

--- src/app.config.js
+++ src/app.config.js
@@ -1,8 +1,9 @@
-routing.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
+routing.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider', '$httpProvider'];
 
 export default function routing($locationProvider, $stateProvider, $urlRouterProvider) {
 
     //$locationProvider.html5Mode(true);
+
     $urlRouterProvider.otherwise('/404');
 
     $stateProvider
@@ -12,7 +13,10 @@ export default function routing($locationProvider, $stateProvider, $urlRouterPro
         })
         .state('404', {
             url: '/404',
-            template: '<page-not-found/>'
+            template: '<page-not-found/>',
+            params: {
+                error: null
+            }
         });
 
 }
