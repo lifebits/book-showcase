ac030a0c0f18d0b521dc1722fdcac1791df3e678
Nikolay Popov <mail@lifebits.ru>
2016-05-17 23:37:53 +0700

    add restangular and lodash

--- src/app.js
+++ src/app.js
@@ -6,13 +6,17 @@ import './css/main.scss';
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import ngResource from 'angular-resource';
+import restangular from 'restangular';
+
 import ngMaterial from 'angular-material';
+
+import _ from 'lodash';
 
 import routing from './app.config';
 
 import showcase from './showcase';
 
-angular.module('app', [uiRouter, ngResource, ngMaterial, showcase])
+angular.module('app', [uiRouter, 'restangular', ngResource, ngMaterial, showcase])
     .config(routing)
 
     .directive('checkLoadingImg', require('./checkLoadingImg.directive'))
