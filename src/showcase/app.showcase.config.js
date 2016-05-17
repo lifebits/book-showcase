85907b2d1aea39bfaed41bb7569dc23038e368d9
Nikolay Popov <mail@lifebits.ru>
2016-05-17 23:58:24 +0700

    update goodsResourceInit and add goodsRestangularInit

--- src/showcase/app.showcase.config.js
+++ src/showcase/app.showcase.config.js
@@ -1,15 +1,23 @@
-routing.$inject = ['$stateProvider'];
+routing.$inject = ['$stateProvider', 'RestangularProvider'];
 
-export default function routing($stateProvider) {
+export default function routing($stateProvider, RestangularProvider) {
+
+    RestangularProvider.setBaseUrl('https://ds.aggregion.com/api/public/');
 
     $stateProvider
         .state('showcase', {
             url: '/showcase',
             resolve: {
-                goodsInit: ['BooksService',
-                    function(BooksService) {
-                        return BooksService.getBooksFromServer().then (
-                            result => BooksService.books = result
+                goodsResourceInit: ['BooksResourceService',
+                    function(BooksResourceService) {
+                        return BooksResourceService.getBooksFromServer().then (
+                            result => BooksResourceService.books = result
+                        );
+                    }],
+                goodsRestangularInit: ['BooksRestangularService',
+                    function(BooksRestangularService) {
+                        return BooksRestangularService.getBooksFromServer().then (
+                            response => BooksRestangularService.books = response
                         );
                     }]
             },
@@ -18,21 +26,33 @@ export default function routing($stateProvider) {
         .state('showcase.book', {
             url: '/:bookId',
             resolve: {
-                goodsInit: ['BooksService', '$stateParams',
-                    function(BooksService, $stateParams) {
+                goodsResourceInit: ['BooksResourceService', '$stateParams',
+                    function(BooksResourceService, $stateParams) {
                         let bookId = $stateParams.bookId;
-                        return BooksService.getBookFromServer(bookId).then (
-                            result => BooksService.book = result
+                        return BooksResourceService.getBookFromServer(bookId).then (
+                            result => BooksResourceService.book = result
                         ).then(
-                            () => BooksService.getBundlesBookFromServer(bookId)
+                            () => BooksResourceService.getBundlesBookFromServer(bookId)
                         ).then(
-                            bundles => BooksService.bundles = bundles
+                            bundles => BooksResourceService.bundles = bundles
                         ).catch(error => {
-                            console.log(error);
+                            console.log(error)
                         });
+                    }],
+                goodsRestangularInit: ['BooksRestangularService', '$stateParams',
+                    function(BooksRestangularService, $stateParams) {
+                        let bookId = $stateParams.bookId;
+                        return BooksRestangularService.getBookFromServer(bookId).then(
+                            result => BooksRestangularService.book = result
+                        ).then(
+                            () => BooksRestangularService.getBundlesBookFromServer()
+                        ).then(
+                            bundles => BooksRestangularService.bundles = bundles
+                        ).catch(error => {
+                            console.log(error)
+                        })
                     }]
             },
             template: '<book-info/>'
         });
-
 }
